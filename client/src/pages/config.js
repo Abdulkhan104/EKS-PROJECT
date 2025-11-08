9c0e6bdf69fb92705dd07f90015fd0c7a002fe52
Abdul khan <abdulkhan333c@gmail.com>
2025-11-09 04:00:34 +0530

    Update API_BASE_URL to new endpoint

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://3.80.67.98:86";
+const API_BASE_URL = "http://aa5dbf42dd04c4cc1827ab36a17532ba";
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
